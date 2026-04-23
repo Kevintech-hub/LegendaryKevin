a3508254f535368003efa453b4af761152531fd0
Kevintech-hub <techkevin93@gmail.com>
2026-04-23 22:00:53 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,115 @@
+/* m
+  -! Credits By Kevintech 
+  Thanks to great lonelysaam 
+  Thanks to Malvin King 
+  https://wa.me/256742932677
+*/
+
+// setting/config.js
+const fs = require('fs');
+
+// --- Setting Owner ---?  
+ //  
+global.owner = ["256742932677"];  
+global.sudo = ["256742932677", "256755585369"];// Type additional allowed users here
+//NB: They'll be able to use every functions of the bot without restrictions.
+global.ownername = "Kelvin Tech";  
+global.botname = "JEXPLOIT";  
+
+// ========= Setting Channel ========= //
+global.namachannel = "KEVIN";
+global.idchannel = "120363398454335106@newsletter";
+global.linkchannel = "";
+
+// ========= Setting Status ========= //
+global.antispam = true;
+global.autoread = false;
+global.autoreact = false;
+global.antibug = true;
+global.autobio = false;
+global.autoTyping = false;
+global.autorecording = false;
+global.prefixz = '.';
+
+// ========= Anti-Delete Feature ========= //
+global.antidelete = 'private'; // Options: 'private', 'chat', or 'off'
+
+// ===== Anticall ===========
+global.anticall = 'off';// options :- 'off', 'decline' or 'block'
+// off - Disables anticall
+// decline - Declines incoming calls
+// Block - Declines and blocks callers
+
+// ======= Anti-Edit ==============
+global.antiedit = 'private'; // options: 'private, 'chat', or 'off'
+
+// ====== Global for status ========
+global.autoviewstatus = 'true';    // Enable auto-view status
+global.autoreactstatus = 'true';   // Enable auto-react to status  
+global.statusemoji = '💚';         // Emoji to use for reactions
+
+// ======Antilink globals=======°°
+global.antilinkdelete = true;
+global.antilinkwarn = true;
+global.antilinkkick = false;
+
+
+// ========= Other Global Settings ========= //
+global.SESSION_ID = process.env.SESSION_ID || '';
+global.postgresqls = process.env.DATABASE_URL || "";
+
+global.welcome = true;
+global.adminevent = true;
+global.AI_CHAT = "false"; // Set to "true" to enable AI chatbot by default
+
+
+// ========= Add modeStatus and versions ========= //
+global.modeStatus = "Public";
+global.versions = "1.4.8";
+
+// ========= Setting WM ========= //
+global.packname = 'Jexploit';
+global.author = 'Bot';
+global.wm = '©Jexploit is awesome 🔥';
+
+// === For only developer ============
+global.api = "https://apiskeith.top";
+global.wwe = "https://www.wwe.com/api/news";
+global.wwe1 = "https://www.thesportsdb.com/api/v1/json/3/searchfilename.php?e=wwe";
+global.wwe2 = "https://www.thesportsdb.com/api/v1/json/3/searchevents.php?e=wrestling";
+global.falcon = "https://flowfalcon.dpdns.org";
+
+global.gcount = {
+  prem: 500,
+  user: 15
+};
+
+global.limitCount = 10;
+
+global.mess = {
+  group: "*This is not a group*",
+  notadmin: "This command is only preserved for group admins only!",
+  owner: "Haha😆, I will not respond because your not my owner",
+  done: "*Operation succeeded*",
+  notext: "*Please provide the necessary text*",
+  premium: "*First become a premium user*",
+  botadmin: "Please bot needs admins permission!",
+  error: "An error occurred while processing the command!",
+  limited: "*Limit reached*",
+  helpersList: [
+    { name: "Malvin king", number: "+263776388689", country: "Zimbabwe", flag: "🇿🇼" },
+    { name: "𝐊𝐚𝐚𝐗𝐇𝐮𝐧𝐭𝐞𝐫𝐳", number: "+91 80751 69545", country: "India", flag: "🇮🇳" },
+    { name: "Terri", number: "+256752792178", country: "Uganda", flag: "🇺🇬" },
+    { name: "༅᭄𖣐∭•𝐑𝐎𝐌𝐀-𝐓𝐄𝐂𝐇•∭𖣐᭄༅", number: ",256791480644", country: "Uganda", flag: "🇺🇬" },
+    { name: "Lord Voyage", number: "+256702662846", country: "Uganda", flag: "🇺🇬" }
+  ],
+  siputzx: "https://api.siputzx.my.id" 
+};
+
+
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  delete require.cache[file];
+  require(file);
+});
